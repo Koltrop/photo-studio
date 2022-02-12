e0cc2e10bd620a2a15ac3c2daae4dcdfdb931e70
Daniel Castro <koltropsuriel@gmail.com>
2022-02-12 23:31:57 +0300

    animaciones en sala-1 y contact

---
+++ js/animation.js
@@ -0,0 +1,51 @@
+var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+        triggerElement: "#trigger1",
+        triggerHook: 0.8,
+        offset: 50
+    })
+    .setClassToggle("#reveal1", "visible")
+    //.addIndicators()
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+        triggerElement: "#trigger2",
+        triggerHook: 0.8,
+        offset: 50
+    })
+    .setClassToggle("#reveal2", "visible")
+    //.addIndicators()
+    .addTo(controller);
+
+var revealElements = document.getElementsByClassName("digit");
+for (var i = 0; i < revealElements.length; i++) {
+    new ScrollMagic.Scene({
+            triggerElement: revealElements[i],
+            offset: 50,
+            triggerHook: 0.8,
+        })
+        .setClassToggle(revealElements[i], "visible")
+        /*.addIndicators({
+            name: "digit " + (i + 1)
+        })*/
+        .addTo(controller);
+}
+
+new ScrollMagic.Scene({
+        triggerElement: "#trigger3",
+        triggerHook: 0.8,
+        offset: 50,
+    })
+    .setClassToggle("#reveal3", "visible")
+    //.addIndicators()
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+        triggerElement: "#trigger4",
+        triggerHook: 0.8,
+        offset: 50,
+    })
+    .setClassToggle("#reveal4", "visible")
+    //.addIndicators()
+    .addTo(controller);
